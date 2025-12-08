21c2e59ead35ceb66065b538b8abb90eacbd1fb7
nexstar-simonzouain <C-SZouain@nexstar.tv>
2025-12-08 18:47:51 +0000

    Moved .gitignore to root and fixed the describe.

--- tests/hooks-challenge.spec.ts
+++ tests/hooks-challenge.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('<replace_this_with_your_title>', async ({ page }) => {
+test.describe('<replace_this_with_your_title>', () => {
   // Candidate's solution goes here
 });
