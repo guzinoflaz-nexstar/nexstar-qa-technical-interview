bc0a8ea68b7deb4fbcc08e97f986626b6e2d632b
nexstar-simonzouain <C-SZouain@nexstar.tv>
2026-01-08 17:44:58 +0000

    Included steps inside the spec files.

--- tests/hooks-challenge.spec.ts
+++ tests/hooks-challenge.spec.ts
@@ -1,3 +1,14 @@
+/**
+ * Test requirements:
+
+    Create a test suite that defines a SITE_URL variable set to: https://www.theguardian.com/international
+    Add a setup hook that verifies the site responds with a 200 status code.
+    Add a test case that clicks the site logo in the navigation bar.
+    Assert the value of SITE_URL matches the current URL.
+    Add a teardown hook that takes a screenshot.
+    The test must have the tag @test_hooks
+ */
+
 import { test, expect } from '@playwright/test';
 
 test.describe('<replace_this_with_your_title>', () => {
--- tests/pom-challenge.spec.ts
+++ tests/pom-challenge.spec.ts
@@ -1,3 +1,13 @@
+/**
+ * Test requirements:
+
+    Create a test that relies on a Page Object Model with the base URL: https://www.theguardian.com/international
+    Create a navigation bar component that is accessible from the Page Object.
+    Add a class method that navigates to a section using a value from a custom data structure representing navigation sections (e.g. News, Opinion, Sport, etc.)
+    Assert the page URL matches the section passed to the method.
+    The test must have the tag @test_pom
+ */
+
 import { test, expect } from '@playwright/test';
 
 test('page object model challenge', async ({ page }) => {
